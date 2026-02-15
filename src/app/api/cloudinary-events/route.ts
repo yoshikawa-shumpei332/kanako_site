ba52e4c122c12b3555fd8ad6b95fe5e77d79a272
yoshikawa-shumpei332 <yoshikawa.shumpei231@gmail.com>
2026-02-16 07:55:48 +0900

    runtime="edge"を追加

--- src/app/api/cloudinary-events/route.ts
+++ src/app/api/cloudinary-events/route.ts
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from 'next/server'
-
+export const runtime = 'edge';
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
